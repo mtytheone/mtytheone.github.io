75fc96aff3b780f2c5e5c23f03cfb8e6627318ee
The_O <39901162+mtytheone@users.noreply.github.com>
2020-02-01 15:11:39 +0900

    [Delete] extra files and codes.

--- test/test1.js
+++
@@ -1,9 +0,0 @@
-var $box = '.box';
-var $count = '.count';
-var count = 1;
-
-$box.on('transitionend', function(e){
-  if (e.originalEvent.propertyName === 'width') {
-    $count.text(count++);
-  }
-});
